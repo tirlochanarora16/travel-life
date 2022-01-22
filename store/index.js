40a3a8ceac664122780cef132bdcae93f88702f2
tirlochanarora16 <tirlochan.arora16@gmail.com>
2022-01-22 18:40:59 +0530

    Removed unused files and modules

--- store/index.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import formSlice from "./form-slice";
-
-const store = configureStore({
-    reducer: { form: formSlice.reducer }
-})
-
-export default store;
